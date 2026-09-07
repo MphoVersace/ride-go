b4a8575f97e317b3c5a778603f1939ff53ddc04d
MphoVersace <mtlomelompho@gmail.com>
2026-09-07 20:45:04 +0200

    feat: add circular road and car

--- App.js
+++ App.js
@@ -8,6 +8,11 @@ export default function App() {
 
       <Text style={styles.logo}>RideGo</Text>
 
+      <View style={styles.road}>
+        <Text style={styles.car}>🚗</Text>
+        <View style={styles.innerCircle} />
+      </View>
+
       <Text style={styles.tagline}>Fast. Safe. Reliable.</Text>
     </View>
   );
@@ -25,11 +30,35 @@ const styles = StyleSheet.create({
     fontSize: 48,
     fontWeight: "bold",
     color: "#5BC0FF",
-    marginBottom: 20,
+    marginBottom: 35,
+  },
+
+  road: {
+    width: 220,
+    height: 220,
+    borderRadius: 110,
+    borderWidth: 28,
+    borderColor: "#1D3557",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+
+  car: {
+    position: "absolute",
+    top: -32,
+    fontSize: 32,
+  },
+
+  innerCircle: {
+    width: 164,
+    height: 164,
+    borderRadius: 82,
+    backgroundColor: "#071A3D",
   },
 
   tagline: {
     fontSize: 18,
     color: "#FFFFFF",
+    marginTop: 35,
   },
 });
