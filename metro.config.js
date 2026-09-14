4d062a5e2d04566081dcf678e48d6ae3d2e65e52
thulanesigasa <pharezsigasa@gmail.com>
2026-09-14 02:50:14 +0200

    chore: remove React Native and TypeScript codebase, migrate exclusively to native Kotlin

--- metro.config.js
+++
@@ -1,8 +0,0 @@
-const { getDefaultConfig } = require("expo/metro-config");
-
-const config = getDefaultConfig(__dirname);
-
-// Enable Metro bundler to resolve 3D model asset extensions
-config.resolver.assetExts.push("glb", "gltf", "obj", "usdz");
-
-module.exports = config;
